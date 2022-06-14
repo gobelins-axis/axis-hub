f8aa70203cf30d38cd00152fc70643f8c0d383ef
Léo Generet <lpgeneret@gmail.com>
2022-06-14 02:18:26 +0200

    :construction: Edit page is responsive but value problems

--- pages/games/edit/_id.vue
+++ pages/games/edit/_id.vue
@@ -1,88 +1,188 @@
 <template>
-    <div>
+    <form class="form-container template" @submit="submitHandler">
+        <div class="panel left">
+            <div class="section">
+                <div class="section-title">Informations</div>
+                <div class="field">
+                    <input ref="inputName" type="text" placeholder="Nom du jeu"
+                           :value="`${getSelectGameDatas.fields.name}`">
+                </div>
 
-        <form class="update-form" @submit="submitHandler">
-            <h1>{{ getSelectGameDatas.fields.name }}</h1>
+                <div class="field">
+                    <input id="year" ref="year" type="number" placeholder="Année de création"
+                           :value="`${getSelectGameDatas.fields.year}`">
+                </div>
 
-            <div>
-                <label for="nameU">New game</label>
-                <input id="nameU" ref="newNameU" name="name" type="text" :value="`${getSelectGameDatas.fields.name}`">
+                <div class="field">
+                    <input id="players" ref="players" type="number"
+                           placeholder="Nombre de joueurs min." :value="`${getSelectGameDatas.fields.players}`">
+                </div>
+
+                <div class="field">
+                    <textarea id="credits" ref="credits" type="textarea"
+                              placeholder="Crédits" :value="`${getSelectGameDatas.fields.credits}`"></textarea>
+                </div>
+
+                <div class="field">
+                    <textarea id="description" ref="inputDescription" type="textarea" rows=3
+                              maxlength="160" minlength="10"
+                              placeholder="Description courte (160 char. max)"
+                              :value="`${getSelectGameDatas.fields.description}`"></textarea>
+                </div>
+
+                <div class="field">
+                    <textarea id="descriptionLong" ref="inputDescriptionLong" name="descriptionLong" rows=6
+                              type="textarea"
+                              maxlength="900" minlength="100"
+                              placeholder="Description longue (900 char. max)"
+                              :value="`${getSelectGameDatas.fields.longerDescription}`"></textarea>
+                </div>
             </div>
 
-            <div>
-                <label for="description">Description</label>
-                <input id="description" ref="inputDescription" name="description" type="text" :value="`${getSelectGameDatas.fields.description}`" >
+            <div class="section">
+                <div class="field">
+                    <fieldset ref="tags">
+                        <div class="checkbox">
+                            <label>
+                                <span class="checkbox-title">Solo</span>
+                                <input ref="onePlayer" type="checkbox" name="filter" class="default__check"
+                                       value="Solo" :checked=getSelectGameDatas.fields.filters.onePlayer>
+                                <span class="custom__check"></span>
+                            </label>
+                        </div>
+                        <div class="checkbox">
+                            <label>
+                                <span class="checkbox-title">Multijoueurs</span>
+                                <input ref="multiPlayer" type="checkbox" name="filter" class="default__check"
+                                       value="Multijoueur" :checked=getSelectGameDatas.fields.filters.multiPlayer>
+                                <span class="custom__check"></span>
+                            </label>
+                        </div>
+                        <div class="checkbox">
+                            <label>
+                                <span class="checkbox-title">Experience</span>
+                                <input ref="experience" type="checkbox" name="filter" class="default__check"
+                                       value="Experience" :checked=getSelectGameDatas.fields.filters.experience>
+                                <span class="custom__check"></span>
+                            </label>
+                        </div>
+                        <div class="checkbox">
+                            <label>
+                                <span class="checkbox-title">Jeu</span>
+                                <input ref="game" type="checkbox" name="filter" class="default__check" value="Jeu"
+                                       :checked=getSelectGameDatas.fields.filters.game>
+                                <span class="custom__check"></span>
+                            </label>
+                        </div>
+                    </fieldset>
+                </div>
+
+                <div class="field flex leaderboard">
+                    <label>Afficher le tableau des scores</label>
+                    <label>
+                        <input id="leaderboard" ref="leaderboard" name="leaderboard" type="checkbox"
+                               class="default__check switchbox"
+                               :checked=getSelectGameDatas.fields.leaderboardActive>
+                        <span class="custom__check"></span>
+                    </label>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="panel right">
+            <div class="section">
+                <div class="section-title">Projet</div>
+                <div class="field">
+                    <input id="file" ref="url" name="file" type="text" placeholder="Insérer une URL"
+                           :value="`${getSelectGameDatas.fields.url}`">
+                    <input @focus="$event.target.select()" readonly id="tokenID" ref="tokenID" name="tokenID"
+                           type="text"
+                           :value="`${getSelectGameDatas.fields.id}`">
+                    <ButtonSimple class="token-id-btn" @click.native="copyToClipboard" ref="tokenIDBtn"
+                                  text="Copier l'ID" type="styled" color="yellow"/>
+                </div>
             </div>
 
-            <div>
-                <label for="url">URL</label>
-                <input id="url" ref="inputUrl" name="url" type="text" :value="`${getSelectGameDatas.fields.url}`">
+            <div class="section">
+                <div class="section-title">Images</div>
+                <div class="image-section">
+                    <div class="image-wrapper medium" ref="medium">
+                        <div class="inputContainer">
+                            <input
+                                id="mediumImage"
+                                ref="mediumImage"
+                                type="file"
+                                name="mediumImage"
+                                accept="image/png, image/jpeg"
+                                @change="addedMediumImage">
+
+                            <img v-if="mediumImageAdded" class="previewImage" ref="previewMediumImage"
+                                 src="" alt=""/>
+                            <ButtonSimple text="Aperçu liste (1200x470px)" class="overlay" icon="download"
+                                          theme="light"/>
+
+                        </div>
+                    </div>
+
+                    <div class="image-wrapper large" ref="large">
+                        <div class="inputContainer">
+                            <input
+                                id="largeImage"
+                                ref="largeImage"
+                                type="file"
+                                name="largeImage"
+                                accept="image/png, image/jpeg"
+                                @change="addedLargeImage"
+                            >
+
+                            <img v-if="largeImageAdded" class="previewImage" ref="previewLargeImage"
+                                 src="" alt=""/>
+                            <ButtonSimple text="Visuel plein écran (2560x1440) " class="overlay" icon="download"/>
+
+                        </div>
+                    </div>
+                </div>
             </div>
 
-            <div>
-                <label for="url">Players</label>
-                <input id="players" ref="players" name="players" type="number" :value="`${getSelectGameDatas.fields.players}`">
+            <div class="section">
+                <div class="section-title">Couleurs LEDS du menu</div>
+                <div ref="colors" class="colorsContainer">
+                    <div class="colorContainer">
+                        <div class="label">Couleur 1</div>
+                        <div class="color-picker-container color1">
+                            <div class="hexa-code">{{ color1 }}</div>
+                            <div class="preview" :style="{backgroundColor: color1 } "></div>
+                            <verte v-model="color1" picker="square" model="hex"
+                                   value="`${getSelectGameDatas.fields.colors.secondary}`"></verte>
+                        </div>
+                    </div>
+
+                    <div class="colorContainer">
+                        <div class="label">Couleur 2</div>
+                        <div class="color-picker-container color1">
+                            <div class="hexa-code">{{ color2 }}</div>
+                            <div class="preview" :style="{backgroundColor: color2 } "></div>
+                            <verte v-model="color2" menuPosition="top" picker="square" model="hex"
+                                   value="`${getSelectGameDatas.fields.colors.secondary}`"></verte>
+                        </div>
+                    </div>
+                </div>
             </div>
 
-            <div>
-                <label for="url">Credits</label>
-                <input id="credits" ref="credits" name="credits" type="textarea" maxlength="300" :value="`${getSelectGameDatas.fields.credits}`">
+            <div class="action-buttons edit">
+                <div class="cancel-edit">Annuler les modifications</div>
+                <div class="second-row">
+                    <img class="delete-btn" src="~/assets/images/delete.svg" alt="" @click="deleteHandler"/>
+                    <ButtonSimple @click.native="submitHandler" ref="submitButton"
+                                  :text="success ? 'Jeu mis à jour 👍🏼' : error ? 'Verifiez les infos 🚫' : 'Sauvegarder' "
+                                  type="styled"
+                                  :success="success" :error="error"/>
+                </div>
             </div>
-
-            <div>
-                <label for="url">Active leaderboard</label>
-                <input id="leaderboard" ref="leaderboard" name="leaderboard" type="checkbox" :value="`${getSelectGameDatas.fields.leaderboardActive}`">
-            </div>
-
-            <div>
-                <label for="mediumImage">Medium image</label>
-                <input
-                    id="mediumImage"
-                    ref="mediumImage"
-                    type="file"
-                    name="mediumImage"
-                    accept="image/png, image/jpeg"
-                >
-            </div>
-
-            <div>
-
-                <label for="largeImage">Large image</label>
-                <input
-                    id="largeImage"
-                    ref="largeImage"
-                    type="file"
-                    name="largeImage"
-                    accept="image/png, image/jpeg"
-                >
-            </div>
-
-            <div>
-                <label for="filters">Filters</label>
-                <fieldset>
-                    <input ref="onePlayer" type="checkbox" name="filter" value="Solo" checked="`${getSelectGameDatas.fields.onePlayer}`" >Solo<br>
-                    <input ref="multiPlayer" type="checkbox" name="filter" value="Multijoueur" checked="`${getSelectGameDatas.fields.multiPlayer}`">Multijoueurs<br>
-                    <input ref="experience" type="checkbox" name="filter" value="Experience" checked="`${getSelectGameDatas.fields.experience}`">Experience<br>
-                    <input ref="game" type="checkbox" name="filter" value="Jeu" checked="`${getSelectGameDatas.fields.game}`">Jeu<br>
-                </fieldset>
-            </div>
-
-            <div>
-                <label for="filters">Colors</label>
-                <input ref="color1" value="#ff0000">
-                <input ref="color2" value="#ff0000">
-            </div>
-
-            <input type="submit" value="Submit">
-
-
-
-        </form>
-
-        <button @click="deleteHandler" >DELETE</button>
-
-    </div>
+        </div>
+    </form>
 </template>
 
 <script src="./script.js"></script>
-<style src="./style.scss" lang="scss" scoped></style>
+<style src="./../create/style.scss" lang="scss"></style>
