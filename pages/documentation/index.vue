d0c83d17052e70374b1bf5a08f0104d4b32e8d71
leochocolat <23156630+leochocolat@users.noreply.github.com>
2022-08-29 18:33:54 +0200

    :sparkles: Setup page documentation

---
+++ pages/documentation/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="page-documentation">
+
+    </div>
+</template>
+
+<script src="./script.js"></script>
+<style src="./style.scss" lang="scss" scoped></style>
---
+++ pages/documentation/script.js
@@ -0,0 +1,24 @@
+// Mixins
+import seo from '@/mixins/seo';
+import pageTransitions from '@/mixins/pageTransitions';
+
+export default {
+    mixins: [seo, pageTransitions],
+
+    data() {
+        return {};
+    },
+
+    methods: {
+        /**
+         * Public
+         */
+        transitionIn(done, routeInfos) {
+            if (done) done();
+        },
+
+        transitionOut(done, routeInfos) {
+            if (done) done();
+        },
+    },
+};
