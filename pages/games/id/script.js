f85ee14707e3eb482c5f181f20d652fbf9baeefe
leochocolat <23156630+leochocolat@users.noreply.github.com>
2022-08-30 12:44:19 +0200

    :construction: WIP page game detail

--- pages/games/id/script.js
+++ pages/games/id/script.js
@@ -1,12 +1,32 @@
+// Vendor
+import { mapGetters } from 'vuex';
+
 // Mixins
 import seo from '@/mixins/seo';
 import pageTransitions from '@/mixins/pageTransitions';
+
+// Components
+import Footer from '@/components/Footer';
 
 export default {
     mixins: [seo, pageTransitions],
 
     data() {
         return {};
+    },
+
+    computed: {
+        ...mapGetters({
+            games: 'games/games',
+        }),
+
+        game() {
+            const game = this.games.find((item) => {
+                return item.id === this.$route.params.id;
+            });
+
+            return game;
+        },
     },
 
     methods: {
@@ -20,5 +40,13 @@ export default {
         transitionOut(done, routeInfos) {
             if (done) done();
         },
+    },
+
+    components: {
+        Footer,
+    },
+
+    validate({ params, store }) {
+        return store.state.games.games.some(game => game.id === params.id);
     },
 };
