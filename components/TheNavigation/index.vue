3457ce6961fa5b1aded061b1f4c8353ac75da375
leochocolat <23156630+leochocolat@users.noreply.github.com>
2022-10-15 17:19:58 +0200

    :sparkles: Add github link on logged out navigation

--- components/TheNavigation/index.vue
+++ components/TheNavigation/index.vue
@@ -31,9 +31,13 @@
         <!-- Not authentificated Nav -->
         <div v-else class="buttons-container">
 
-            <nuxt-link class="button button-navigation" :to="localePath($utils.localeCopy.routes.documentation.path)">
+            <!-- <nuxt-link class="button button-navigation" :to="localePath($utils.localeCopy.routes.documentation.path)">
                 {{ $utils.localeCopy.routes.documentation.name }}
-            </nuxt-link>
+            </nuxt-link> -->
+
+            <a class="button button-navigation" href="http://github.com/gobelins-axis/axis-api" target="_blank" rel="noopener noreferrer">
+                {{ $utils.localeCopy.routes.documentation.name }}
+            </a>
 
             <nuxt-link class="button button-navigation" :to="localePath($utils.localeCopy.routes.games.path)">
                 {{ $utils.localeCopy.routes.games.name }}
